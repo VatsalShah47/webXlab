9e02e5b0f6101117ed1ffcc093adb3c5559d5c9c
VatsalShah47 <111197601+VatsalShah47@users.noreply.github.com>
2023-02-21 11:32:26 +0530

    Add files via upload

---
+++ 2 - helloworld/mathematical.js
@@ -0,0 +1,7 @@
+var a = 12;
+var b = 7;
+console.log('Addition: ' + a + ' + ' + b + ' = ' + (a + b));
+console.log('Subtraction: ' + a + ' - ' + b + ' = ' + (a - b));
+console.log('Multiplication: ' + a + ' * ' + b + ' = ' + (a * b));
+console.log('Division: ' + a + ' / ' + b + ' = ' + (a / b));
+console.log('Modulo: ' + a + ' % ' + b + ' = ' + (a % b));
---
+++ 2 - helloworld/mathematical.ts
@@ -0,0 +1,7 @@
+let a : number = 12;
+let b : number = 7;
+console.log('Addition: ' + a + ' + ' + b + ' = ' + (a+b));
+console.log('Subtraction: ' + a + ' - ' + b + ' = ' + (a-b));
+console.log('Multiplication: ' + a + ' * ' + b + ' = ' + (a*b));
+console.log('Division: ' + a + ' / ' + b + ' = ' + (a/b));
+console.log('Modulo: ' + a + ' % ' + b + ' = ' + (a%b));
